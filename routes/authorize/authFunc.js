3610b0170e36a8bbc7259c7a0098d2206023b7ed
Seungmin <haradwaith@naver.com>
2022-02-25 17:25:02 +0900

    for test only

--- routes/authorize/authFunc.js
+++ routes/authorize/authFunc.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 function authFunc(req, res, next) {
     try {
         const { token } = req.headers;
+        console.log('token', token);
+        console.log('header', req.headers);
         // { id, nickname } 꼴로 locals.user에 들어있음.
         const { id } = jwt.verify(token, 'secret');
         res.locals = { id }
