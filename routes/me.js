355835bc5376fcaeb8de28e3dc60290896719869
Seungmin <haradwaith@naver.com>
2022-02-25 12:00:49 +0900

    [fix]

--- routes/me.js
+++ routes/me.js
@@ -3,19 +3,23 @@ const jwt = require('jsonwebtoken');
 const router = require('express')();
 
 router.get('/', async (req, res) => {
-    const { id } = res.locals;
-    if (!id) {
+    const { token } = req.headers;
+    try {
+        const { id } = jwt.verify(token, 'secret');
+    } catch (err) {
         res.send({
             success: false,
             errorMessage: '로그인 후 이용해주세요'
         });
         return;
     }
+
     const user = await User.findOne({
         where: { id },
         attributes: [nickname, profile_img_url]
     });
     res.send({
+        success: true,
         id,
         nickname: user.nickname,
         profile_img_url: user.profile_img_url
